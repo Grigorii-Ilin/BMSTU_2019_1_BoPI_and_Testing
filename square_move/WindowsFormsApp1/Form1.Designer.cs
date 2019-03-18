c7c03440026e942531a97e36918b352b1500ecc2
Grigorii Ilin <37904148+Grigorii-Ilin@users.noreply.github.com>
2019-03-18 22:05:35 +0300

    added Lab1
    A square moves like in arcanoid. We need to catch it

---
+++ square_move/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btStart = new System.Windows.Forms.Button();
+            this.tmOneMove = new System.Windows.Forms.Timer(this.components);
+            this.lbXCoord = new System.Windows.Forms.Label();
+            this.lbYCoord = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(655, 26);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(75, 23);
+            this.btStart.TabIndex = 1;
+            this.btStart.TabStop = false;
+            this.btStart.Text = "Старт";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStartStop_Click);
+            // 
+            // tmOneMove
+            // 
+            this.tmOneMove.Interval = 40;
+            this.tmOneMove.Tick += new System.EventHandler(this.tmSquareMove_Tick);
+            // 
+            // lbXCoord
+            // 
+            this.lbXCoord.AutoSize = true;
+            this.lbXCoord.Location = new System.Drawing.Point(687, 79);
+            this.lbXCoord.Name = "lbXCoord";
+            this.lbXCoord.Size = new System.Drawing.Size(0, 13);
+            this.lbXCoord.TabIndex = 1;
+            // 
+            // lbYCoord
+            // 
+            this.lbYCoord.AutoSize = true;
+            this.lbYCoord.Location = new System.Drawing.Point(690, 105);
+            this.lbYCoord.Name = "lbYCoord";
+            this.lbYCoord.Size = new System.Drawing.Size(0, 13);
+            this.lbYCoord.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbYCoord);
+            this.Controls.Add(this.lbXCoord);
+            this.Controls.Add(this.btStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Двигающийся квадрат";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Timer tmOneMove;
+        private System.Windows.Forms.Label lbXCoord;
+        private System.Windows.Forms.Label lbYCoord;
+    }
+}
+
---
+++ square_move/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private int x;
+        private int y;
+        private int vectorX = 1;
+        private int vectorY = 1;
+
+
+        private const int SQUARE_LEN = 10;
+        private const int SPEED = 5;
+
+        private int bridgeX;
+        private const int BRIDGE_WIDTH = 50;
+        private const int BRIDGE_SPEED = 9;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.Green, x, y, SQUARE_LEN, SQUARE_LEN);
+            e.Graphics.FillRectangle(Brushes.Red, bridgeX, this.Height - 47, BRIDGE_WIDTH, SQUARE_LEN);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            x = 50;
+            y = 50;
+            bridgeX = 200;
+        }
+
+        private void tmSquareMove_Tick(object sender, EventArgs e)
+        {
+            if (x <= 0 || x >= this.Width - 25)
+            {
+                vectorX *= -1;
+            }
+            else if (y <= 0 || y >= this.Height - 42)
+            {
+                vectorY *= -1;
+            }
+            else if (y >= this.Height - 50 && x >= bridgeX && x <= bridgeX + BRIDGE_WIDTH)
+            {
+                vectorY *= -1;
+            }
+
+            x += SPEED * vectorX;
+            y += SPEED * vectorY;
+            lbXCoord.Text = "X: " + x.ToString();
+            lbYCoord.Text = "Y: " + y.ToString();
+
+
+
+            Invalidate();
+        }
+
+        private void btStartStop_Click(object sender, EventArgs e)
+        {
+            tmOneMove.Enabled = !(tmOneMove.Enabled);
+            btStart.Enabled = false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                bridgeX -= BRIDGE_SPEED;
+            }
+            else if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                bridgeX += BRIDGE_SPEED;
+            }
+        }
+    }
+}
